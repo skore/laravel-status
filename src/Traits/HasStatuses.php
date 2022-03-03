531371a2aa202005f330be5c1c99be5268316a03
Rubén Robles <d8vjork@outlook.com>
2022-03-03 14:15:36 +0100

    Fix creating event propagation

--- src/Traits/HasStatuses.php
+++ src/Traits/HasStatuses.php
@@ -22,8 +22,6 @@ trait HasStatuses
         if (config('status.enable_events', true)) {
             static::creating(function ($model) {
                 event(new StatusCreating($model));
-
-                return true;
             });
         }
     }
